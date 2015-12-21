fcc59323d0bb5a59fb9ff02e10ffde094d39047c
Nick Strayer <nstrayer@uvm.edu>
2015-12-20 21:04:30 -0500

    Okay, everyhing should be working now. There is some wonky double slash stuff going on with the url but that's okay

--- build.R
+++ build.R
@@ -3,19 +3,17 @@ local({
   baseurl = servr:::jekyll_config('.', 'baseurl', '/')
   knitr::opts_knit$set(base.url = baseurl)
   # fall back on 'kramdown' if markdown engine is not specified
-  # markdown = servr:::jekyll_config('.', 'markdown', 'kramdown')
+  markdown = servr:::jekyll_config('.', 'markdown', 'kramdown')
   # see if we need to use the Jekyll render in knitr
-  # if (markdown == 'kramdown') {
-  #   knitr::render_jekyll()
-  # } else knitr::render_markdown()
-
-  knitr::render_jekyll()
+  if (markdown == 'kramdown') {
+    knitr::render_jekyll()
+  } else knitr::render_markdown()
 
   # input/output filenames are passed as two additional arguments to Rscript
   a = commandArgs(TRUE)
   d = gsub('^_|[.][a-zA-Z]+$', '', a[1])
   knitr::opts_chunk$set(
-    fig.path   = sprintf('assets/%s/', d),
+    fig.path   = sprintf('figures/%s/', d),
     cache.path = sprintf('cache/%s/', d)
   )
 
